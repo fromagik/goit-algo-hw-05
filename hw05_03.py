de21c843e04e11562302ffbb6515887718e0411a
Slavik <yaroslav.salko71@gmail.com>
2024-06-22 01:28:56 +0200

    function filter_logs_by_level almost ready

--- hw05_03.py
+++ hw05_03.py
@@ -1,33 +1,35 @@
-# for line in lst:
-#     list_line = line.split(' ')
-#     if list_line[2] == "INFO":
-#         print(list_line[0] + ' ' + list_line[1] + ' - ' + " ".join(list_line[3:]))
-from pathlib import Path
+import os
+import sys
+
 
 def parse_log_line(line: str) -> dict:
     new_line = line.strip().split()
-    date, log, disc = new_line[:2], new_line[2:3], new_line[3:]
+    date, time, log, disc = new_line[:1], new_line[1:2], new_line[2:3], new_line[3:]
     new_dict = {
-        'Date' : date,
-        'Log' : log,
-        'disc' : disc
+        'Date' : " ".join(date),
+        'Time' : " ".join(time),
+        'Log' : ' '.join(log),
+        'disc' : " ".join(disc)
     }
     return new_dict
 
 def load_logs(file_path: str) -> list:
     lin = []
-    if file_path.is_file():
-        with file_path.open('r') as file:
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
             lines = file.readlines()
             for line in lines:
                 parse_log_line(line)
+                line = line.strip()
+                lin.append(line)
 
-
-file_path = Path('logs.txt')
-load_logs(file_path)
+    return lin
 
 def filter_logs_by_level(logs: list, level: str) -> list:
-    pass
+    print(f"Деталі логів для рівня '{level}':")
+    for log in logs:
+        if level in log:
+            print(log)
 
 
 def count_logs_by_level(logs: list) -> dict:
@@ -37,3 +39,11 @@ def count_logs_by_level(logs: list) -> dict:
 def display_log_counts(counts: dict):
     pass
 
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+        level = sys.argv[2]
+        filter_logs_by_level(load_logs(path), level)
+    else: 
+        path = os.getcwd()
